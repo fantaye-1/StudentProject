139ff24678818f7ee2c9a48f75cb46eefb9f3ae8
Tara Kelly <kellyt2@students.westerntc.edu>
2020-06-24 07:29:56 -0500

    changed Address test file to AddressRunner and added MainMenu class with outline

---
+++ MainMenu.java
@@ -0,0 +1,34 @@
+
+public class MainMenu {
+
+	public static void main(String[] args) {
+		
+		runScenario();
+		System.out.println("Would you like to go back to main menu? Y or N");
+		if(Y) {
+			main(args);
+		}
+
+	}
+	
+	public static void runScenario() {
+		
+		// display main menu and ask for option:
+		// What would you like to do? Select option below:
+			//1)Get all students  2) Find a student 3) Add a new student 4) Update a student 5) Remove a student
+
+		// use a switch case (or loop) to account for their inputs of 1-5
+		// run methods below based on options 1-5
+	}
+	
+	public static void displayGetAllStudents(){}
+
+	public static void displayFindAStudent(){}
+
+	public static void displayAddAStudent(){}  
+
+	public static void displayUpdateAStudent(){} 
+
+	public static void displayDeleteAStudent(){}
+
+}
