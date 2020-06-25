a009243f8272231ceb5a47097aa6317f32eb39bf
Tara Kelly <kellyt2@students.westerntc.edu>
2020-06-25 07:03:35 -0500

    some more comments and bug fixes. this should be most updated version of project

--- JDBC/src/com/cognixia/jump/jdbc/project/MainMenu.java
+++ JDBC/src/com/cognixia/jump/jdbc/project/MainMenu.java
@@ -1,77 +1,106 @@
+package com.cognixia.jump.jdbc.project;
 import java.sql.Date;
+import java.util.List;
 import java.util.Scanner;
 
 import com.cognixia.jump.jdbc.dao.Department;
 import com.cognixia.jump.jdbc.dao.DepartmentDAOImp;
-import com.cognixia.jump.jdbc.project.Address;
-import com.cognixia.jump.jdbc.project.AddressDAOImp;
-import com.cognixia.jump.jdbc.project.Student;
-import com.cognixia.jump.jdbc.project.StudentDAOImp;
 
 public class MainMenu {
 
 	public static void main(String[] args) {
-
-		// Where to put Scanner open and close is tricky (I may need to use a while loop
-		// instead of recalling method)
+		
+		// Where to put Scanner open and close is tricky (I may need to use a while loop instead of recalling method)
 		// I'll start by opening and closing inside main and passing it to runScenario
 		// I don't know if Scanner is OK like this but it's working
 		Scanner input = new Scanner(System.in);
 		runScenario(input);
-
+		
 		System.out.println("Would you like to go back to main menu? Y or N");
 		String answer = input.next();
-
-		if (answer.equalsIgnoreCase("Y")) {
+		
+		if(answer.equalsIgnoreCase("Y")) {
 			main(args);
-		} else {
+		}else {
+			System.out.println("Thank you. Goodbye.");
 			input.close();
 			System.exit(0);
 		}
-
+		
 	}
-
+	
 	public static void runScenario(Scanner input) {
-
+		
 		// display main menu and ask for option:
 		// What would you like to do? Select option below:
-		// 1)Get all students 2) Find a student 3) Add a new student 4) Update a student
-		// 5) Remove a student
+			//1)Get all students  2) Find a student 3) Add a new student 4) Update a student 5) Remove a student
 
 		// use a switch case to account for their inputs of 1-5
 		// run methods below based on options 1-5
-
+		
 		System.out.println("What would you like to do? Select option below:");
-		System.out.println(
-				"1) Get all students \t2) Find a student \t3) Add a new student\t 4) Update a student\t5) Remove a student");
+		System.out.println("1) Get all students \t2) Find a student \t3) Add a new student\t 4) Update a student\t5) Remove a student");
 		String choice = input.nextLine();
-
-		switch (choice) {
-		case "1":
-			displayGetAllStudents(input);
-			break;
-		case "2":
-			displayFindAStudent(input);
-			break;
-		case "3":
-			displayAddAStudent(input);
-			break;
-		case "4":
-			displayUpdateAStudent(input);
-			break;
-		case "5":
-			displayDeleteAStudent(input);
-			break;
+		
+		switch(choice) {
+			case "1":
+				displayGetAllStudents(input);
+				break;
+			case "2":
+				displayFindAStudent(input);
+				break;
+			case "3":
+				displayAddAStudent(input);
+				break;
+			case "4":
+				displayUpdateAStudent(input);
+				break;
+			case "5":
+				displayDeleteAStudent(input);
+				break;
 		}
 	}
+	
+	// Larkin
+	public static void displayGetAllStudents(Scanner input){
+		
+		// need to use StudentDAO - TK
+		//List<Student> students = getAllStudents();
+		
+		StudentDAOImp studentDao = new StudentDAOImp();
+		List<Student> students = studentDao.getAllStudents();
+		System.out.println("All Students:\n");
 
-	public static void displayGetAllStudents(Scanner input) {
+		for (Student s : students) {
+			System.out.println(s);
+		}
+
+		System.out.println("\n");
 	}
 
-	public static void displayFindAStudent(Scanner input) {
+	// Larkin
+	public static void displayFindAStudent(Scanner input){
+		
+		System.out.println("Enter ID to find student:");
+
+		// don't call this student, call id or studentId -TK
+		// best to take input as a string and then parse to an integer-
+		// it needs to be an integer to use getStudentById
+		//String student = input.nextLine();
+		String studentIdStr = input.nextLine();
+		int studentId = Integer.parseInt(studentIdStr);
+		// you have to have this line of code to use any methods from student DAO
+		StudentDAOImp studentDao = new StudentDAOImp();
+		Student displayStudent = studentDao.getStudentById(studentId);
+		
+
+		System.out.println("Information on " + displayStudent + ":\n");			
+		System.out.println(displayStudent);
+	
 	}
 
-	public static void displayAddAStudent(Scanner input) {
+	// me
+	public static void displayAddAStudent(Scanner input){
 		
 		// To add a student, first need to create a new Address object
 		// could be split into methods further (displayNewAddress, displayChooseDepartment, etc.)
@@ -79,7 +108,7 @@ public class MainMenu {
 		
 		// Also need to assign the new student to a department
 		Department studentDept = displayChooseDepartment(input);
-
+		
 		// now can take rest of input for new student
 		System.out.println("Enter student first name:");
 		String firstName = input.nextLine().trim();
@@ -93,19 +122,22 @@ public class MainMenu {
 		System.out.println("Enter number of student credits:");
 		String credStr = input.nextLine().trim();
 		int credits = Integer.parseInt(credStr);
-
+		
 		Student newStudent = new Student(0, firstName, lastName, gender, dob, credits, newAddress, studentDept);
-		System.out.println("Student ready to add to DB: " + newStudent);
-
-		// TODO: still need addStudent()
-		// newStudent = studentDAO.addStudent(newStudent);
-		// syso("Student was added: " + newStudent)
 		
-	}
-
-	public static void displayUpdateAStudent(Scanner input) {
+		StudentDAOImp studentDao = new StudentDAOImp();
+		boolean added = studentDao.addStudent(newStudent);
+		if(added) {
+			System.out.println("Student was added to DB w/ inserted ID: " + newStudent);
+		}else {
+			System.out.println("Error: student was not added");
+		}
 		
+	}  
 
+	// Abrham
+	public static void displayUpdateAStudent(Scanner input){
+		
 		System.out.println("------updating students--------");
 		// ask the id
 		System.out.println("Enter the Student Id");
@@ -188,15 +220,45 @@ public class MainMenu {
 		} else {
 			System.out.println("The info is not updated");
 		}		
-	}
+	} 
 
-	public static void displayDeleteAStudent(Scanner input) {
+	// Abrham
+	public static void displayDeleteAStudent(Scanner input){
+		
+		// get id from user
+		System.out.println("------- Deleting Student---------");
+		System.out.println("Enter the Student Id: ");
+
+		int std_id = input.nextInt();
+		// TODO chek the id is between 1000
+
+		// get the updated student info and call delete method.
+		StudentDAOImp student = new StudentDAOImp();
+		boolean delt_std = student.deleteStudent(std_id);
+
+		if (delt_std) {
+			System.out.println("Sucess fully deleted");
+
+		} else {
+			System.out.println("The info is not deleted");
+		}	
+				
 	}
 	
-	// helper methods for displayAddAStudent
+	// Abrham's helper method
+	public static int checkStudentId(int std_id,Scanner input){
+		int id =std_id;
+		while(!(id >= 10000 && id <= 1000000) || id == 0) {
+			System.out.println("wrong student id");
+			id=input.nextInt();
+		}
+		return std_id;
+	}
+	
+	// helper methods for displayAddAStudent - Tara
 	public static Address displayAddAnAddress(Scanner input) {
-
-		System.out.println("Enter student address:");
+		
+		System.out.println("Enter student address:\n---------");
 		System.out.println("Enter street name:\n");
 		String street = input.nextLine().trim();
 		System.out.println("Enter city name:\n");
@@ -212,13 +274,14 @@ public class MainMenu {
 		newAddress = addressDao.addAddress(newAddress);
 		return newAddress;
 	}
-
+	
+	// Tara
 	public static Department displayChooseDepartment(Scanner input) {
-
+		
 		System.out.println("Choose a department id to assign to new student:\n");
 		DepartmentDAOImp deptDao = new DepartmentDAOImp();
 		// could pretty-print this
-		for (Department dept : deptDao.getAllDepartments()) {
+		for(Department dept : deptDao.getAllDepartments()) {
 			System.out.println(dept);
 		}
 		System.out.println("(Enter department id:)\n");
--- JDBC/src/com/cognixia/jump/jdbc/project/StudentDAOImp.java
+++ JDBC/src/com/cognixia/jump/jdbc/project/StudentDAOImp.java
@@ -1,79 +1,198 @@
 package com.cognixia.jump.jdbc.project;
 
+import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
+
+import com.cognixia.jump.jdbc.connection.ConnectionManagerProperties;
+import com.cognixia.jump.jdbc.dao.DepartmentDAO;
+import com.cognixia.jump.jdbc.dao.DepartmentDAOImp;
 
 public class StudentDAOImp implements StudentDAO {
 
+	private Connection conn = ConnectionManagerProperties.getConnection();
+	
+	// Abrham
 	@Override
 	public List<Student> getAllStudents() {
-		// TODO Auto-generated method stub
-		return null;
+		List<Student> studentList = new ArrayList<>();
+
+		// find all students
+		try (Statement stmt = conn.createStatement(); ResultSet rs = stmt.executeQuery("select * from Student");) {
+
+			while (rs.next()) {
+
+				int id = rs.getInt(1);
+				String firstName = rs.getString(2);
+				String lastName = rs.getString(3);
+				String gender = rs.getString(4);
+				Date dob = rs.getDate(5);
+				int credits = rs.getInt(6);
+				int address_id = rs.getInt(7);
+				// TODO need to call the address get address method for @AddressDAOImp AND
+				// change the null
+				AddressDAO address = new AddressDAOImp();
+				// address.getAddressById(address_id);
+
+				int dep = rs.getInt(8);
+				// TODO need to call the department get department method for @departmentDAOImp
+				// AND change the null
+				DepartmentDAO depa = new DepartmentDAOImp();
+				depa.getDepartmentById(dep);
+				// add to list
+				Student std = new Student(id, firstName, lastName, gender, dob, credits,
+						address.getAddressById(address_id), depa.getDepartmentById(dep));
+				studentList.add(std);
+
+			}
+
+		} catch (SQLException e) {
+
+			e.printStackTrace();
+		}
+
+		return studentList;
 	}
 
+	// Abrham
 	@Override
-	public Student getStudentById(int id) {
-		// TODO Auto-generated method stub
-		return null;
+	public Student getStudentById(int std_id) {
+		Student std = null;
+
+		// select * from department where dept_id = ?
+		try (PreparedStatement sstmt = conn.prepareStatement("select * from student where student_id = ?")) {
+
+			sstmt.setInt(1, std_id);
+
+			ResultSet rs = sstmt.executeQuery();
+
+			if (rs.next()) {
+
+				int id = rs.getInt(1);
+				String firstName = rs.getString(2);
+				String lastName = rs.getString(3);
+				String gender = rs.getString(4);
+				Date dob = rs.getDate(5);
+				int credits = rs.getInt(6);
+				int address_id = rs.getInt(7);
+				AddressDAO address = new AddressDAOImp();
+				// address.getAddressById(address_id);
+				int dep = rs.getInt(8);
+				DepartmentDAO depa = new DepartmentDAOImp();
+				// depa.getDepartmentByID(dep);
+
+				std = new Student(id, firstName, lastName, gender, dob, credits, address.getAddressById(address_id),
+						depa.getDepartmentById(dep));
+
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		return std;
 	}
 
 	@Override
 	public boolean addStudent(Student student) {
-		// insert into student values(id, fname, lname, gender, dob, credits, address_id, dept_id);
+		try {
+			
+			PreparedStatement sstmt = conn.prepareStatement("insert "
+				+ "into student values(?,?,?,?,?,?,?,?)");
+			
+			sstmt.setInt(1, student.getId());
+			sstmt.setString(2, student.getFirstName());
+			sstmt.setString(3, student.getLastName());
+			// error: this should be gender -TK
+			//sstmt.setString(4, student.getLastName());
+			sstmt.setString(4, student.getGender());
+			sstmt.setDate(5, student.getDob());
+			sstmt.setInt(6, student.getCredits());
+			sstmt.setInt(7, student.getAddress().getId());
+			// error: this was 7, needs to be 8 -TK
+			sstmt.setInt(8, student.getDept().getId());
+			
+			int insert = sstmt.executeUpdate();
+			
+			if(insert > 0) {
+				return true;
+			}
+			
+			sstmt.close();
+			
+		} catch (SQLException e) {
+			
+			e.printStackTrace();
+		}
 		
-		// grab the id for department
-		int deptId = student.getDept().getId();
-		
-		// grab id for address
-		int addressId = student.getAddress().getId();
-		
-		// add in values to statement
 		
 		return false;
 	}
 
+	// Abrham
 	@Override
 	public boolean updateStudent(Student student) {
-		// id, fname, lname, gender, dob, credits, addr_id, dept_id);
+		//id, fname, lname, gender, dob, credits, addr_id, dept_id);
 		// credits was missing from statement -TK
-		try (PreparedStatement sstmt = conn.prepareStatement("update student set first_name = ?, last_name = ?,"
+		try(PreparedStatement sstmt = conn.prepareStatement("update student set first_name = ?, last_name = ?,"
 				+ " gender = ?, date_of_birth =?, credits =?, address_id=?, dept_id=? where student_id = ?");) {
-
+			
 			// this was marked as 9 and I changed to 8, in case wondering of errors -TK
 			sstmt.setInt(8, student.getId());
-
+			
 			sstmt.setString(1, student.getFirstName());
 			sstmt.setString(2, student.getLastName());
 			// was missing line for gender and so rest of numbers were off, changed -TK
-			sstmt.setString(3, student.getGender());
+			sstmt.setString(3,  student.getGender());
 			sstmt.setDate(4, student.getDob());
 			sstmt.setInt(5, student.getCredits());
 			sstmt.setInt(6, student.getAddress().getId());
-			// grab the id for the address // int addrId = student.getAddress().getId();
-//					AddressDAOImp addr = new AddressDAOImp();
-//					addr.updateAddress(student.getAddress().getId());
-
-			// grab the id for the department // int deptId = student.getDept().getId();
+			// grab the id for the address	//	int addrId = student.getAddress().getId();
+//			AddressDAOImp addr = new AddressDAOImp();
+//			addr.updateAddress(student.getAddress().getId());
+			
+			
+			// grab the id for the department //	int deptId = student.getDept().getId();
 			// this was 6 and should be 7 -TK
-			sstmt.setInt(7, student.getDept().getId());
+			sstmt.setInt(7, student.getDept().getId());					
 			int update = sstmt.executeUpdate();
-
-			if (update > 0) {
+			
+			if(update > 0) {
 				return true;
-			}
-
+			}					
+			
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-
+		
 		return false;
 	}
 
+	// Abrham
 	@Override
 	public boolean deleteStudent(int id) {
-		// TODO Auto-generated method stub
+		
+		// error: statement had dept_id in where clause instead of student_id -TK
+		try(PreparedStatement sstmt = conn.prepareStatement("delete from student where student_id = ?")) {
+
+			sstmt.setInt(1, id);
+
+			int deleted = sstmt.executeUpdate();
+
+			if (deleted > 0) {
+				return true;
+			}
+
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+
 		return false;
 	}
 
